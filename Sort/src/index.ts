9d61e0c8c7e443b9b51061b69e2168a803637f35
ttuom1nen <ttuominen@outlook.com>
2020-03-02 21:41:24 +0200

    Implemented abstract class

--- Sort/src/index.ts
+++ Sort/src/index.ts
@@ -4,13 +4,11 @@ import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+numbersCollection.sort();
 console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection("XaaYT");
-const sorterb = new Sorter(charactersCollection);
-sorterb.sort();
+charactersCollection.sort();
 console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
@@ -19,6 +17,5 @@ linkedList.add(-10);
 linkedList.add(-1);
 linkedList.add(12);
 
-const sorterc = new Sorter(linkedList);
-sorterc.sort();
+linkedList.sort();
 linkedList.print();
