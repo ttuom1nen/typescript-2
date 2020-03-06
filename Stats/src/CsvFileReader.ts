6a71a8f47659f892ed24358880c731e5c57a31da
ttuom1nen <ttuominen@outlook.com>
2020-03-06 21:08:30 +0200

    Added MatchData tuple

--- Stats/src/CsvFileReader.ts
+++ Stats/src/CsvFileReader.ts
@@ -1,8 +1,11 @@
 import fs from "fs";
 import { dateStringToDate } from "./utils";
+import { MatchResult } from "./MatchResult";
+
+type MatchData = [Date, string, string, number, number, MatchResult, string];
 
 export class CsvFileReader {
-  data: string[][] = [];
+  data: MatchData[] = [];
 
   constructor(public filename: string) {}
 
@@ -22,7 +25,9 @@ export class CsvFileReader {
           row[1],
           row[2],
           parseInt(row[3]),
-          parseInt(row[4])
+          parseInt(row[4]),
+          row[5] as MatchResult,
+          row[6]
         ];
       });
   }
