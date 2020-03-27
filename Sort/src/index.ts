990598ca7623cb566d745bc939f74a6600d9aa49
ttuom1nen <ttuominen@outlook.com>
2020-03-27 15:01:04 +0200

    Removed unused import

--- Sort/src/index.ts
+++ Sort/src/index.ts
@@ -1,4 +1,3 @@
-import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
