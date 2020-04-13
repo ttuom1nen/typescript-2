c25c61442ddceb57616b3d2b12f7cfd64c5a8454
ttuom1nen <ttuominen@outlook.com>
2020-04-13 22:01:10 +0300

    Added some more decorators

--- decorators.ts
+++ decorators.ts
@@ -1,15 +1,38 @@
+@classDecorator
 class Boat {
+  @testDecorator
   color: string = "red";
 
+  @testDecorator
   get formattedColor(): string {
     return `this boats color is ${this.color}`;
   }
 
   @logError("Oops the boat sank!")
-  pilot(): void {
-    throw new Error();
-    console.log("swish");
+  pilot(
+    @parameterDecorator speed: string,
+    @parameterDecorator generateWake: boolean
+  ): void {
+    //throw new Error();
+
+    if (speed === "fast") {
+      console.log("swish");
+    } else {
+      console.log("nothing");
+    }
   }
+}
+
+function classDecorator(constructor: typeof Boat) {
+  console.log(constructor);
+}
+
+function parameterDecorator(target: any, key: string, index: number) {
+  console.log(key, index);
+}
+
+function testDecorator(target: any, key: string) {
+  console.log(key);
 }
 
 function logError(errorMessage: string) {
