acf07f3e5133819cafc59ee1416f9410aa5632bd
ttuom1nen <ttuominen@outlook.com>
2020-04-16 19:49:07 +0300

    Added another example

--- metadata.ts
+++ metadata.ts
@@ -18,3 +18,27 @@ console.log(height); // 10
 // Get object property:
 const planeColor = Reflect.defineMetadata("note", "hi there", plane, "color");
 console.log(planeColor);
+
+/*----------*/
+
+class Plane {
+  color: string = "red";
+
+  @markFunction("secret information!")
+  fly(): void {
+    console.log("vrrrr");
+  }
+}
+
+function markFunction(secretInfo: string) {
+  return function (target: Plane, key: string) {
+    Reflect.defineMetadata("secret", secretInfo, target, key);
+  };
+}
+
+function printMetadata(target: typeof Plane) {
+  for (let key in target.prototype) {
+    const secret = Reflect.getMetadata("secret", target.prototype, key);
+    console.log(secret);
+  }
+}
